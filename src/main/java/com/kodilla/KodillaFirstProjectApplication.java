819272e1267d576e3882eb74b448d40f3c68304c
Grzegorz Krajewski <g.krajewski82@gmail.com>
2021-12-03 13:26:22 +0100

    first commit

---
+++ src/main/java/com/kodilla/KodillaFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaFirstProjectApplication.class, args);
+    }
+
+}
